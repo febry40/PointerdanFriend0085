6dde99eecce9708a3b25e2de1698382b96c744f9
febry40 <febryevayonisa@gmail.com>
2024-05-06 16:18:45 +0700

    membuat class pelajar

--- Pointerlima.cpp
+++ Pointerlima.cpp
@@ -7,3 +7,23 @@ public:
     void showNilaiPelajar(pelajar &x);
 };
 
+class pelajar
+{
+private:
+    int nilai;
+
+public:
+    pelajar() { nilai = 100;}
+    friend void manusia::showNilaiPelajar(pelajar &x);
+};
+
+void manusia::showNilaiPelajar(pelajar &x)
+{
+    std::cout << x.nilai;
+}
+int main()
+{
+    manusia budi;
+    pelajar budi;
+    budi.showNilaiPelajar(pbudi);
+    return 0;
