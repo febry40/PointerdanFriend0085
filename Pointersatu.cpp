99879c02e3fb4649a754bc41b53e7a7e26a6ddac
febry40 <febryevayonisa@gmail.com>
2024-05-06 16:08:33 +0700

    pointer dua selesai

---
+++ Pointersatu.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa {
+public:
+	int nim;
+	void shownim() {
+		cout << "No Induk = " << nim << endl;
+	}
+};
+
+int main() {
+
+	mahasiswa mhs{ 1 };
+	mhs.shownim();
+
+	mahasiswa& refMhs = mhs;
+	refMhs.nim = 2;
+	mhs.shownim();
+
+	mahasiswa* pMhs = &mhs;
+	pMhs->nim = 3;
+	pMhs->shownim();
+	return 0;
+}
